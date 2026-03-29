25ba84a97562cd58cdbc005f338d5181b2779d08
Paul <built.x.bad@gmail.com>
2026-03-29 15:45:48 +0100

    Allow modal to open for Bluesky/Mastodon even if already connected - enables updating credentials

--- src/app/feeds/page.tsx
+++ src/app/feeds/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import Image from 'next/image';
-import CredentialModal from '../components/CredentialModal';
+import CredentialModal from './components/CredentialModal';
 
 export default function FeedsPage() {
   const [dragOver, setDragOver] = useState(false);
